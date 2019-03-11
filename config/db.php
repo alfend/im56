8912577bf6ab2fc88d75296245ad4676fc9842a0
alfend <alfend@yandex.ru>
2019-03-11 16:55:56 +0500

    urok5

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=alfendyan2_im56',
+    'username' => 'alfendyan2_im56',
+    'password' => 'Dp7cmim56',
     'charset' => 'utf8',
 ];
