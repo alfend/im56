f8db4fa87e0102a7873039465506c0ea3629f586
alfend <alfend@yandex.ru>
2019-03-14 11:22:10 +0500

    urok8

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=alfendyan2_im56',
-    'username' => 'alfendyan2_im56',
-    'password' => 'Dp7cmim56',
-    'charset' => 'utf8',
-];
